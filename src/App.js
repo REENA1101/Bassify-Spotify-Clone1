08c33c5cc28abad9e5ee58d8bb427f3e8cd16880
Reena kumari <reenakvs1@gmail.com>
2023-07-15 14:13:17 +0530

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Login from './Components/Login'
+import { useEffect } from 'react'
 
 export default function App() {
+
+useEffect(()=>{
+   const hash = window.location.hash
+   if(hash){
+    const token = hash.substring(1).split("&")[0].split("=")[1];
+    console.log(token)
+   }
+},[])
+
   return (
     <div>
       <Login/>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,20 @@ import styled from "styled-components"
 
 export default function Login() {
    const handleClick=()=>{
-    alert ("YOU ARE CONNECTED TO SPOTIFY")
+      const clientId = "0e717170d3664c14b9037ffc957474c2";
+      const redirectUrl =  "http://localhost:3000/";
+      const apiUrl= "https://accounts.spotify.com/authorize"
+      const scope = ['user-read-email',
+        'user-read-private', 
+        'user-read-playback-state',
+        'user-modify-playback-state',
+        'user-read-currently-playing',
+        'user-read-playback-position',
+        'user-top-read',
+        'user-read-recently-played']
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+          " "
+          )}&response_type=token&show-dialogue=true`
    }
 
   return (
