11f271433aa45d988f3d9bd6269cbd3598e7d0fa
Reena kumari <reenakvs1@gmail.com>
2023-11-11 16:16:08 +0530

    Working on playerControl component and volume component Error 403

--- src/Components/Volume.jsx
+++ src/Components/Volume.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import styled from "styled-components";
+import {useStateProvider} from '../utils/StateProvider';
 
 export default function Volume() {
+  const [{token}] = useStateProvider()
+
+   const setVolume = e => {
+
+   }
+
   return (
-    <Container>Volume</Container>
+    <Container>
+      <input type="range" min={0} max={100} onMouseUp={(e=>setVolume(e))} />
+    </Container>
   )
 }
 
 const Container = styled.div`
+display : flex;
+justify-content: flex-end;
+align-items: 
 `
